271725028a28c923ab7d53b397fd2e4947b14444
imanninen <igor.manninen@gmail.com>
2023-12-17 13:09:18 +0200

    add compiled library file

--- lib/src/main/kotlin/fslibrary/Utils.kt
+++ lib/src/main/kotlin/fslibrary/Utils.kt
@@ -1,6 +1,6 @@
 package fslibrary
 
-fun hasSameNames(testFolder: FSFolder): Boolean {
+internal fun hasSameNames(testFolder: FSFolder): Boolean {
     val map = testFolder.content.map { it.name to testFolder.content.count { its -> its.name == it.name } }
     map.forEach { (_, count) ->  if (count > 1) return true }
     return false
